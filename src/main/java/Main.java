3bc3938570c381aee5d60ffc9e6bb6f2caa8270e
Manczak Adrian <manczakadrian@gmail.com>
2021-05-17 19:28:57 +0200

    update

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,24 +1,11 @@
-import drive.Drive;
-import drive.HDDDrive;
-import drive.SSDDrive;
-import file.imagefile.GIFImageFile;
-import file.imagefile.JPGImageFile;
 import file.musicfile.MP3MusicFile;
-
-import java.io.File;
 
 public class Main {
     public static void main(String[] args) {
-        File gif1 = new File("nazwa1.gif", String.valueOf(100));
-        JPGImageFile jpg1 = new JPGImageFile("nazwa1.jpg", 200, 80);
-        MP3MusicFile mp3file = new MP3MusicFile("plik.mp3", 4000, "Marelin Manson", "Broken", 100);
 
-        Drive drive = new HDDDrive();
-        drive.addFile(gif1);
+        MP3MusicFile mp3MusicFile = new MP3MusicFile("plik.mp3", 20,"Cool Kids","Butelki",20  );
 
-    drive.listFiles();
-       // File file = drive.findFile("nazwa1.gif");
-
+        System.out.println(mp3MusicFile.getName());
     }
 
 }
--- src/main/java/file/musicfile/MP3MusicFile.java
+++ src/main/java/file/musicfile/MP3MusicFile.java
@@ -5,7 +5,7 @@ public class MP3MusicFile extends AbstractMusicFile{
     private int quality;
 
 
-    public MP3MusicFile(String name, int size, String bandName, String title, int quality) {
+    public MP3MusicFile(String name, int size, String bandName,String title, int quality) {
         super(name, size, bandName, title);
 
         this.quality = quality;
